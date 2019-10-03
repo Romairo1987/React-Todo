698936678c919cee0b67bdc52db006211695b7e3
Romairo1987 <49536760+Romairo1987@users.noreply.github.com>
2019-10-02 18:23:21 -0700

    finsihed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,82 @@
 import React from 'react';
+import TodoList from "./TodoComponents/TodoList"
+import Todo from "./TodoComponents/Todo"
+import TodoForm from "./TodoComponents/TodoForm"
+import '../src/TodoComponents/Todo.css'
+
+
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+ constructor(){
+   super()
+   this.state={
+     whatodo:TodoList
+   }
+ }
+
+
+ toggleItem = (event, itemId) => {
+  event.preventDefault()
+  this.setState({
+    whatodo: this.state.whatodo.map(item => {
+      if (item.id === itemId) {
+        return {
+          ...item, 
+          completed: !item.completed
+        }
+      } else {
+        return item
+      }
+    })
+  })
+}
+
+
+
+clearCompleted = event => {
+  event.preventDefault()
+  console.log(event)
+  this.setState({
+    whatodo: this.state.whatodo.filter(item => {
+      return !item.completed
+    })
+  })
+}
+
+addItem = (event, itemName) => {
+  const newItem = {
+    id: Date.now(),
+    name: itemName,
+    completed: false
+  }
+
+  this.setState({
+    whatodo: [newItem, ...this.state.whatodo]
+  })
+}
+
+  
+  
   render() {
     return (
       <div>
+        <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm addItem={this.addItem}/>
+        </div>
+
+        <div>
+        {this.state.whatodo.map(item => (
+            <Todo
+              key={item.id}
+              item={item}
+              onClick={(e) => this.toggleItem(e, item.id)}
+            />
+          ))}
+
+          <button  onClick={this.clearCompleted}> Clear</button>
+        </div>
+       
       </div>
     );
   }
